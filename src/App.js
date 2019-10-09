9d13ce3160e1c3c978a6addbc3e9ae0ca412d4f7
camila-espitia <camila.espitia@gmail.com>
2019-10-09 18:30:47 -0500

    commit uno

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 
+
+
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      valorTarea: ""
+    }
+  }
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.list.map((valorLista) =>
+              <li key={valorLista}>{valorLista}</li>
+            )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form>
+            <input type="text" value={this.state.valorTarea} onKeyPress={this.agregarTarea.bind(this)} onChange={this.cambiarValor.bind(this)} id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+          </form>
         </div>
       </div>
     )
   }
-}
 
+  cambiarValor(event) {
+    this.setState({
+      valorTarea: event.target.value
+    });
+  }
+
+  agregarTarea(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      let lista = this.state.list;
+      lista.push(this.state.valorTarea)
+      this.setState({
+        valorLista: lista
+      })
+    }
+  }
+}
 
 export default App;
